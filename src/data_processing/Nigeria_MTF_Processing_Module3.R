4092a02ce884a69279b048ac3a910c1e57da2ba0
Joshua Lin <jtlin@berkeley.edu>
2021-08-14 18:25:32 -0700

    clean module 3 data

---
+++ src/data_processing/Nigeria_MTF_Processing_Module3.R
@@ -0,0 +1,76 @@
+# Nigeria MTF
+library(tidyverse)
+library(tidyr)
+#library(RCMIP5)
+library(haven)
+library(readstata13)
+library(foreign)
+library(sjlabelled)
+library(expss)
+library(seplyr)
+library(fastDummies)
+library(purrr)
+setwd("~/Catalyst/MTF_Nigeria/src/data_processing/raw_data") #Change file path for personal use
+
+haven_read <- function(file_name, read_factor = 1){
+  if(read_factor == 1){
+    df <- haven::as_factor(haven::read_dta(file_name))
+  }else{
+    df <- haven::read_dta(file_name)
+  }
+  names(df) <- tolower(names(df))
+  return(df)
+
+}
+stata_read <- function(file_name){
+  df <- readstata13::read.dta13(file_name)
+  names(df) <- tolower(names(df))
+  return(df)
+
+}
+source_use_freq <- function(test_str, var_name){
+  freqTemp <-
+    mtf2[,str_detect(question_labs, test_str)] %>%
+    rename(var_name = 1)
+  freqTemp <- cbind(mtf_info,freqTemp)
+  freqTemp %>%
+    group_by(!! sym(var_name)) %>%
+    summarise(total = n())
+  return(freqOut)
+}
+
+# Read STATA files and combine into a Large List
+StataList <- list.files(pattern = '*.dta')
+merged_StataList <- lapply(list.files(pattern="*.dta",recursive=FALSE, full.names=TRUE), readstata13::read.dta13, nonint.factors = TRUE)
+nga_dta<- haven_read('NGA.dta')
+
+# names(nga_dta)[names(nga_dta) == "hh_id"] <- "HH_ID"
+nga_dta <- nga_dta %>%
+  mutate(locality = ifelse(nga_dta$rur == 'Rural', 1, 0))
+elc_aggr_tier <- nga_dta[,c(16,22,1)]
+
+# View the first data set, "MTF_HH_SEC_C_BATTERY.dta"
+# test returns the sub dataset from the list, test2 returns the dataset with questions tagged.
+test <- data.frame(merged_StataList[[33]])
+#test2 <- haven_read('MTF_HH_SEC_C_BATTERY.dta')
+
+# Try to combine list of 33 elements - DO NOT RUN
+#test2 <- data.frame(matrix(unlist(merged_StataList), nrow=2, byrow=TRUE), stringsAsFactors=FALSE)
+#rm(test2)
+
+#electricity <- data.frame(merged_StataList[[8]])
+electric_assets <- haven_read('MTF_NG_HH_SEC_N_ELEC_ASSET.dta') 
+
+electric_assets <-
+  list(electric_assets, elc_aggr_tier) %>%
+  reduce(inner_join, by='hh_id')
+
+write.csv(electric_assets, '~/Catalyst/MTF_Nigeria/data/nigeria_elec_assets.csv')
+
+finance <- haven_read('MTF_NG_HH_SEC_B.dta')
+
+finance <-
+  list(finance, elc_aggr_tier) %>%
+  reduce(inner_join, by='hh_id')
+
+write.csv(finance, '~/Catalyst/MTF_Nigeria/data/nigeria_finance.csv')
